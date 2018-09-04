a432b1d1a451a58f21d65c6bfc4bfe48f5202bd9
philipstubbs13 <philipstubbs13@gmail.com>
2018-09-04 06:22:41 -0500

    file/directory setup and creating page layouts

--- src/App.js
+++ src/App.js
@@ -1,21 +1,32 @@
+// import React
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import third party routing library to implement navigation within app.
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+// import main app css file.
 import './App.css';
+// import app layout/pages
+import Home from './components/layout/Home';
+import About from './components/layout/About';
+import Work from './components/layout/Work';
+import Contact from './components/layout/Contact';
+import Skills from './components/layout/Skills';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
-  }
+	render() {
+		return (
+			<Router>
+				<div className="App">
+					<Switch>
+						<Route exact path="/" component={Home} />
+						<Route exact path="/about" component={About} />
+						<Route exact path="/work" component={Work} />
+						<Route exact path="/contact" component={Contact} />
+						<Route exact path="/skills" component={Skills} />
+					</Switch>
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
