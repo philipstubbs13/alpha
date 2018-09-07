8a8334e3b344278989bda1486eb9e6d46ce3800b
philipstubbs13 <philipstubbs13@gmail.com>
2018-09-07 03:17:19 -0500

    added background image

--- src/components/layout/Home/Home.js
+++ src/components/layout/Home/Home.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
+// import CSS
+import './Home.css';
 
 class Home extends Component {
 	render() {
 		return (
-			<div>
+			<div className="bg-img">
 				<h1>Hello</h1>
 			</div>
 		);
