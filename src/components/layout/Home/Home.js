9e456ec7d3373d01b193773db00b2e292e8fa7d3
philipstubbs13 <philipstubbs13@gmail.com>
2018-09-07 04:20:14 -0500

    home page content

--- src/components/layout/Home/Home.js
+++ src/components/layout/Home/Home.js
@@ -1,12 +1,26 @@
 import React, { Component } from 'react';
 // import CSS
 import './Home.css';
+// import background image
+import wolf from '../../../images/wolf.jpg';
 
 class Home extends Component {
 	render() {
 		return (
-			<div className="bg-img">
-				<h1>Hello</h1>
+			<div className="home">
+				<img src={wolf} alt="wolf" className="bg" />
+				<header>
+					<h1 className="lg-heading">Phil Stubbs</h1>
+					<h2 className="sm-heading">Code Nerd | Awesome Dev | User Advocate</h2>
+				</header>
+				<main className="home-main">
+					<div className="home-quote">
+						<h2 className="sm-heading">
+							When there are no more leaders to follow, <br />
+							<span className="quote-second-line">you must become one.</span>
+						</h2>
+					</div>
+				</main>
 			</div>
 		);
 	}
