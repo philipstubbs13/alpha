b787088124f20b5824fbbc34aae09a069f5de100
philipstubbs13 <philipstubbs13@gmail.com>
2018-09-07 21:09:32 -0500

    added css grid

--- src/components/layout/Home/Home.js
+++ src/components/layout/Home/Home.js
@@ -10,23 +10,23 @@ class Home extends Component {
 		return (
 			<div className="home">
 				<img src={wolf} alt="wolf" className="bg" />
-				<header>
-					<h1 className="lg-heading">Phil Stubbs</h1>
-					<h2 className="sm-heading">Web Developer, Project Manager, and Writer</h2>
-				</header>
-				<main className="home-main">
-					<div className="home-quote">
+				<div className="home-container">
+					<header className="header">
+						<h1 className="lg-heading">Phil Stubbs</h1>
+						<h2 className="sm-heading">Web Developer, Project Manager, and Writer</h2>
+					</header>
+					<main className="home-main">
 						<h2 className="sm-heading">
 							When there are no more leaders to follow, <br />
 							<span className="quote-second-line">you must become one.</span>
 						</h2>
-					</div>
-					<div className="home-btn">
-						<Link to="/about" className="btn">
-							Learn more about me
-						</Link>
-					</div>
-				</main>
+						<div className="home-btn">
+							<Link to="/about" className="btn">
+								Learn more about me
+							</Link>
+						</div>
+					</main>
+				</div>
 			</div>
 		);
 	}
