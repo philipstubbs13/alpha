6e9ccfcf6f7443f8002b1894b5b4777feff8edab
philipstubbs13 <philipstubbs13@gmail.com>
2018-09-08 05:37:01 -0500

    contact page finished for now

--- src/components/layout/Contact/Contact.js
+++ src/components/layout/Contact/Contact.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 // import CSS
 import './Contact.css';
+// import images
+import Facebook from '../../../images/facebook.svg';
+import LinkedIn from '../../../images/linkedin.svg';
+import Twitter from '../../../images/twitter.svg';
+import GitHub from '../../../images/github.svg';
+import Stackoverflow from '../../../images/stackoverflow.svg';
 
 class Contact extends Component {
 	render() {
@@ -16,16 +22,35 @@ class Contact extends Component {
 					</header>
 					<main className="contact-main">
 						<div className="contact-card">
-							<span>Email: </span>
-							<br />
-							philipstubbs13@gmail.com
+							<span>Email:</span>
+							&nbsp; philipstubbs13@gmail.com
 						</div>
 						<div className="contact-card">
-							<span>Phone: </span>
-							<br />
-							952-454-3933
+							<span>Phone:</span>
+							&nbsp; 952-454-3933
 						</div>
 					</main>
+					<div className="social-icons">
+						<a href="https://www.facebook.com/phil.stubbs.13" target="_blank" rel="noopener noreferrer">
+							<img src={Facebook} alt="Facebook logo" className="social" />
+						</a>
+						<a href="https://twitter.com/iamPhilStubbs" target="_blank" rel="noopener noreferrer">
+							<img src={Twitter} alt="Twitter logo" className="social" />
+						</a>
+						<a href="https://www.linkedin.com/in/philipjstubbs/" target="_blank" rel="noopener noreferrer">
+							<img src={LinkedIn} alt="LinkedIn logo" className="social" />
+						</a>
+						<a href="https://github.com/philipstubbs13" target="_blank" rel="noopener noreferrer">
+							<img src={GitHub} alt="GitHub logo" className="social" />
+						</a>
+						<a
+							href="https://stackoverflow.com/users/8706269/phil"
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							<img src={Stackoverflow} alt="Stackoverflow logo" className="social" />
+						</a>
+					</div>
 				</div>
 			</div>
 		);
