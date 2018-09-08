d7e82f39f00d6c7356ba7bfc466a29dccd336da1
philipstubbs13 <philipstubbs13@gmail.com>
2018-09-08 12:18:57 -0500

    added icons

--- src/components/layout/Work/Work.js
+++ src/components/layout/Work/Work.js
@@ -37,98 +37,176 @@ class Work extends Component {
 					<main className="work-main">
 						<div className="project">
 							<img src={Swag} alt="running with swag website" className="project-image" />
-							<div className="live-link project-btn">Live</div>
-							<div className="project-link project-btn">Project</div>
+							<div className="live-link project-btn">
+								<i className="far fa-eye" /> Live
+							</div>
+							<div className="project-link project-btn">
+								<i className="fab fa-github" /> GitHub
+							</div>
 						</div>
 						<div className="project">
 							<img src={HealthTracker} alt="HealthTracker website" className="project-image" />
-							<div className="live-link project-btn">Live</div>
-							<div className="project-link project-btn">Project</div>
+							<div className="live-link project-btn">
+								<i className="far fa-eye" /> Live
+							</div>
+							<div className="project-link project-btn">
+								<i className="fab fa-github" /> GitHub
+							</div>
 						</div>
 						<div className="project">
 							<img src={Scraper} alt="Web scraper website" className="project-image" />
-							<div className="live-link project-btn">Live</div>
-							<div className="project-link project-btn">Project</div>
+							<div className="live-link project-btn">
+								<i className="far fa-eye" /> Live
+							</div>
+							<div className="project-link project-btn">
+								<i className="fab fa-github" /> GitHub
+							</div>
 						</div>
 						<div className="project">
 							<img src={ClickMe} alt="ClickMe game website" className="project-image" />
-							<div className="live-link project-btn">Live</div>
-							<div className="project-link project-btn">Project</div>
+							<div className="live-link project-btn">
+								<i className="far fa-eye" /> Live
+							</div>
+							<div className="project-link project-btn">
+								<i className="fab fa-github" /> GitHub
+							</div>
 						</div>
 						<div className="project">
 							<img src={MyCookingAssistant} alt="Cooking website" className="project-image" />
-							<div className="live-link project-btn">Live</div>
-							<div className="project-link project-btn">Project</div>
+							<div className="live-link project-btn">
+								<i className="far fa-eye" /> Live
+							</div>
+							<div className="project-link project-btn">
+								<i className="fab fa-github" /> GitHub
+							</div>
 						</div>
 						<div className="project">
 							<img src={SequelBurger} alt="Sequel burger website" className="project-image" />
-							<div className="live-link project-btn">Live</div>
-							<div className="project-link project-btn">Project</div>
+							<div className="live-link project-btn">
+								<i className="far fa-eye" /> Live
+							</div>
+							<div className="project-link project-btn">
+								<i className="fab fa-github" /> GitHub
+							</div>
 						</div>
 						<div className="project">
 							<img src={AdoptMe} alt="pet match website" className="project-image" />
-							<div className="live-link project-btn">Live</div>
-							<div className="project-link project-btn">Project</div>
+							<div className="live-link project-btn">
+								<i className="far fa-eye" /> Live
+							</div>
+							<div className="project-link project-btn">
+								<i className="fab fa-github" /> GitHub
+							</div>
 						</div>
 						<div className="project">
 							<img src={GifFinder} alt="Gif finding website" className="project-image" />
-							<div className="live-link project-btn">Live</div>
-							<div className="project-link project-btn">Project</div>
+							<div className="live-link project-btn">
+								<i className="far fa-eye" /> Live
+							</div>
+							<div className="project-link project-btn">
+								<i className="fab fa-github" /> GitHub
+							</div>
 						</div>
 						<div className="project">
 							<img src={Hangman} alt="Hangman game website" className="project-image" />
-							<div className="live-link project-btn">Live</div>
-							<div className="project-link project-btn">Project</div>
+							<div className="live-link project-btn">
+								<i className="far fa-eye" /> Live
+							</div>
+							<div className="project-link project-btn">
+								<i className="fab fa-github" /> GitHub
+							</div>
 						</div>
 						<div className="project">
 							<img src={Carpool} alt="Carpool matching website" className="project-image" />
-							<div className="live-link project-btn">Live</div>
-							<div className="project-link project-btn">Project</div>
+							<div className="live-link project-btn">
+								<i className="far fa-eye" /> Live
+							</div>
+							<div className="project-link project-btn">
+								<i className="fab fa-github" /> GitHub
+							</div>
 						</div>
 						<div className="project">
 							<img src={Bamazon} alt="Bamazon website" className="project-image" />
-							<div className="live-link project-btn">Live</div>
-							<div className="project-link project-btn">Project</div>
+							<div className="live-link project-btn">
+								<i className="far fa-eye" /> Live
+							</div>
+							<div className="project-link project-btn">
+								<i className="fab fa-github" /> GitHub
+							</div>
 						</div>
 						<div className="project">
 							<img src={Burger} alt="Burger website" className="project-image" />
-							<div className="live-link project-btn">Live</div>
-							<div className="project-link project-btn">Project</div>
+							<div className="live-link project-btn">
+								<i className="far fa-eye" /> Live
+							</div>
+							<div className="project-link project-btn">
+								<i className="fab fa-github" /> GitHub
+							</div>
 						</div>
 						<div className="project">
 							<img src={Crystal} alt="Crystal Collectors game website" className="project-image" />
-							<div className="live-link project-btn">Live</div>
-							<div className="project-link project-btn">Project</div>
+							<div className="live-link project-btn">
+								<i className="far fa-eye" /> Live
+							</div>
+							<div className="project-link project-btn">
+								<i className="fab fa-github" /> GitHub
+							</div>
 						</div>
 						<div className="project">
 							<img src={Liri} alt="Liri website" className="project-image" />
-							<div className="live-link project-btn">Live</div>
-							<div className="project-link project-btn">Project</div>
+							<div className="live-link project-btn">
+								{' '}
+								<i className="far fa-eye" />
+								Live
+							</div>
+							<div className="project-link project-btn">
+								<i className="fab fa-github" /> GitHub
+							</div>
 						</div>
 						<div className="project">
 							<img src={StarWars} alt="Star wars role playing game website" className="project-image" />
-							<div className="live-link project-btn">Live</div>
-							<div className="project-link project-btn">Project</div>
+							<div className="live-link project-btn">
+								<i className="far fa-eye" /> Live
+							</div>
+							<div className="project-link project-btn">
+								<i className="fab fa-github" /> GitHub
+							</div>
 						</div>
 						<div className="project">
 							<img src={Trivia} alt="Super bowl trivia game website" className="project-image" />
-							<div className="live-link project-btn">Live</div>
-							<div className="project-link project-btn">Project</div>
+							<div className="live-link project-btn">
+								<i className="far fa-eye" /> Live
+							</div>
+							<div className="project-link project-btn">
+								<i className="fab fa-github" /> GitHub
+							</div>
 						</div>
 						<div className="project">
 							<img src={ConstructorHangman} alt="Constructor Hangman website" className="project-image" />
-							<div className="live-link project-btn">Live</div>
-							<div className="project-link project-btn">Project</div>
+							<div className="live-link project-btn">
+								<i className="far fa-eye" /> Live
+							</div>
+							<div className="project-link project-btn">
+								<i className="fab fa-github" /> GitHub
+							</div>
 						</div>
 						<div className="project">
 							<img src={TrainBuilder} alt="Train schedule builder website" className="project-image" />
-							<div className="live-link project-btn">Live</div>
-							<div className="project-link project-btn">Project</div>
+							<div className="live-link project-btn">
+								<i className="far fa-eye" /> Live
+							</div>
+							<div className="project-link project-btn">
+								<i className="fab fa-github" /> GitHub
+							</div>
 						</div>
 						<div className="project">
 							<img src={ReactNews} alt="React news website" className="project-image" />
-							<div className="live-link project-btn">Live</div>
-							<div className="project-link project-btn">Project</div>
+							<div className="live-link project-btn">
+								<i className="far fa-eye" /> Live
+							</div>
+							<div className="project-link project-btn">
+								<i className="fab fa-github" /> GitHub
+							</div>
 						</div>
 					</main>
 				</div>
