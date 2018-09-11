c34a8adfdbe12612d980dd9493b4d069b5a2c990
philipstubbs13 <philipstubbs13@gmail.com>
2018-09-11 07:05:37 -0500

    fixed background image

--- src/components/layout/Home/Home.js
+++ src/components/layout/Home/Home.js
@@ -3,42 +3,38 @@ import { Link } from 'react-router-dom';
 // import CSS
 import './Home.css';
 // import background image
-import wolf from '../../../images/wolf.jpg';
+import wolf from '../../../images/wolf3.jpg';
 
 class Home extends Component {
 	render() {
 		return (
 			<div className="home">
-				<div className="home-container">
-					<div className="home-left">
-						<header className="header home-header">
-							<h1 className="lg-heading">
-								Phil <span>Stubbs</span>
-							</h1>
-							<h2 className="sm-heading">Web Developer, Project Manager, and Writer</h2>
-						</header>
-						<main className="home-main">
-							<h2 className="sm-heading">
-								When there are no more leaders to follow, <br />
-								<span className="quote-second-line">you must become one.</span>
-							</h2>
-							<div className="home-nav-links">
-								<Link to="/about" className="btn  home-btn">
-									About Me
-								</Link>
-								<Link to="/work" className="btn home-btn">
-									My Work
-								</Link>
-								<Link to="/contact" className="home-btn btn">
-									Contact Me
-								</Link>
-							</div>
-						</main>
-					</div>
-					<div className="home-right">
-						<img src={wolf} alt="wolf" className="bg" />
-					</div>
+				<div className="home-left">
+					<header className="header home-header">
+						<h1 className="lg-heading">
+							Phil <span>Stubbs</span>
+						</h1>
+						<h2 className="sm-heading">Web Developer, Project Manager, and Writer</h2>
+					</header>
+					<main className="home-main">
+						<h2 className="sm-heading">
+							When there are no more leaders to follow, <br />
+							<span className="quote-second-line">you must become one.</span>
+						</h2>
+						<div className="home-nav-links">
+							<Link to="/about" className="btn  home-btn">
+								About Me
+							</Link>
+							<Link to="/work" className="btn home-btn">
+								My Work
+							</Link>
+							<Link to="/contact" className="home-btn btn">
+								Contact Me
+							</Link>
+						</div>
+					</main>
 				</div>
+				<div className="home-right">{/* <img src={wolf} alt="wolf" className="bg" /> */}</div>
 			</div>
 		);
 	}
